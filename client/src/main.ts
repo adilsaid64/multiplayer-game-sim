b3b1efb152181e86a691f65dd9b7b1b13b98e1c1
adil <adilsaid64@gmail.com>
2026-01-12 22:01:19 +0000

    feat: added a simple game time

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,29 @@
-console.log('hello world')
+const TARGET_FPS = 15
+const FIXED_DT = 1000 / TARGET_FPS
+
+let lastTime = performance.now()
+let accumulator = 0
+
+function update(dt: number) {
+  console.log('hello world', dt)
+}
+
+// param time is passed in from requestAnimationFrame, it represents the time the function was called
+function loop(time: number) {
+  // how much time has elappsed from the start (lastTime) to now (time, when this function is called)
+  let frameTime = (time - lastTime)
+  lastTime = time
+
+  // sum the elappsed time
+  accumulator += frameTime
+
+  // once the elappsed time has passed our threshold, execute the update method
+  while (accumulator >= FIXED_DT) {
+    update(FIXED_DT)
+    accumulator -= FIXED_DT
+  }
+  requestAnimationFrame(loop)
+}
+
+requestAnimationFrame(loop)
+
