99d79bc3ab8a75d56f88397917cad06627f70036
adil <adilsaid64@gmail.com>
2026-01-13 21:03:07 +0000

    chore: setup a formatter

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,21 +1,21 @@
-const TARGET_FPS = 30
-const DELTA_T = 1000 / TARGET_FPS
+const TARGET_FPS = 30;
+const DELTA_T = 1000 / TARGET_FPS;
 
-let tPrev = performance.now()
-let sumDeltaT = 0
+let tPrev = performance.now();
+let sumDeltaT = 0;
 
 function update(deltaT: number) {
-  console.log(deltaT)
+  console.log(deltaT);
 }
 
 function gameLoop(tNow: number) {
-  const tDiff = tNow - tPrev
-  tPrev = tNow
-  sumDeltaT += tDiff
+  const tDiff = tNow - tPrev;
+  tPrev = tNow;
+  sumDeltaT += tDiff;
   while (sumDeltaT >= DELTA_T) {
-    update(DELTA_T)
-    sumDeltaT -= DELTA_T
+    update(DELTA_T);
+    sumDeltaT -= DELTA_T;
   }
-  requestAnimationFrame(gameLoop)
+  requestAnimationFrame(gameLoop);
 }
-requestAnimationFrame(gameLoop)
+requestAnimationFrame(gameLoop);
