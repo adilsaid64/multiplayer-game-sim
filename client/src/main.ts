7f9d6c1a6525e664dc8d1a21627921c6674719c2
adil <adilsaid64@gmail.com>
2026-01-15 21:18:09 +0000

    style: linter

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,6 @@
-import { DELTA_T } from "../../packages/game/constants";
-import { Platform, Player, Game } from "../../packages/game/entities";
-import { update } from "../../packages/game/update";
+import { DELTA_T } from '../../packages/game/constants';
+import { Platform, Player, Game } from '../../packages/game/entities';
+import { update } from '../../packages/game/update';
 
 let tPrev = performance.now();
 let sumDeltaT = 0;
@@ -8,17 +8,17 @@ let sumDeltaT = 0;
 const player = new Player({
   startingPosition: { x: 0, y: 0 },
   size: { x: 5, y: 5 },
-})
+});
 
 const platform = new Platform({
   position: { x: 0, y: 0 },
   size: { x: 10, y: 1 },
-})
+});
 
 const game = new Game({
   players: [player],
-  platforms: [platform]
-})
+  platforms: [platform],
+});
 
 function gameLoop(tNow: number) {
   const tDiff = tNow - tPrev;
