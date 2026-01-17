2c55cf914f2023250b46ed83d6d01317b74486bc
adil <adilsaid64@gmail.com>
2026-01-17 15:40:02 +0000

    feat: added some visuals but AABB collisions not working

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,17 +2,51 @@ import { DELTA_T } from '../../packages/game/constants';
 import { Platform, Player, Game } from '../../packages/game/entities';
 import { update } from '../../packages/game/update';
 
+
+const canvas = document.getElementById('game') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d')!;
+
+function render(game: Game) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = 'blue';
+  for (const platform of game.platforms) {
+    ctx.fillRect(
+      platform.position.x,
+      canvas.height - platform.position.y,
+      platform.size.x,
+      platform.size.y
+    );
+  }
+
+  ctx.fillStyle = 'red';
+  for (const player of game.players) {
+    ctx.fillRect(
+      player.position.x,
+      canvas.height - player.position.y,
+      player.size.x,
+      player.size.y
+    );
+  }
+}
+
+
+
 let tPrev = performance.now();
 let sumDeltaT = 0;
 
 const player = new Player({
-  startingPosition: { x: 0, y: 10 },
-  size: { x: 5, y: 5 },
+  startingPosition: {
+    x: 0, y: 100
+  },
+  size: { x: 20, y: 20 },
 });
 
 const platform = new Platform({
-  position: { x: 0, y: 0 },
-  size: { x: 10, y: 1 },
+  position: {
+    x: 0, y: 50,
+  },
+  size: { x: 300, y: 20 },
 });
 
 const game = new Game({
@@ -28,6 +62,7 @@ function gameLoop(tNow: number) {
     update(DELTA_T, game);
     sumDeltaT -= DELTA_T;
   }
+  render(game);
   requestAnimationFrame(gameLoop);
 }
 requestAnimationFrame(gameLoop);
--- packages/game/update.ts
+++ packages/game/update.ts
@@ -8,23 +8,46 @@ interface GetBoundsArgs {
 interface AABB {
   left: number
   right: number
-  up: number
-  down: number
+  top: number
+  bottom: number
 }
 
 function getBounds(args: GetBoundsArgs): AABB {
   const bounds: AABB = {
-    down: args.entity.position.y + args.entity.size.y / 2,
-    up: args.entity.position.y - args.entity.size.y / 2,
+    top: args.entity.position.y + args.entity.size.y / 2,
+    bottom: args.entity.position.y - args.entity.size.y / 2,
     left: args.entity.position.x - args.entity.size.x / 2,
     right: args.entity.position.x + args.entity.size.x / 2,
   }
   return bounds
 }
 
-function checkCollision(state: Game) {
+interface CheckForIntersectionArgs {
+  entityA: AABB
+  entityB: AABB
+}
+function checkForIntersection(args: CheckForIntersectionArgs): boolean {
+  const cond1 = args.entityA.right > args.entityB.left
+  const cond2 = args.entityA.bottom < args.entityB.top
+  const cond3 = args.entityA.left < args.entityB.right
+  const cond4 = args.entityA.top > args.entityB.bottom
+  // if any are true, there is a intersection
+  return (cond1 || cond2 || cond3 || cond4)
+}
+
+function checkPlayerPlatformCollision(state: Game) {
   for (const player of state.players) {
-    getBounds({ entity: player });
+    const playerBounds = getBounds({ entity: player });
+    for (const platform of state.platforms) {
+      const platformBounds = getBounds({ entity: platform });
+      console.log(player.position, platform.position)
+      console.log(playerBounds, platformBounds)
+      // check intersection
+      if (checkForIntersection({ entityA: playerBounds, entityB: platformBounds })) {
+        console.log('collision found!')
+      } else {
+      }
+    }
   }
 }
 
@@ -36,4 +59,5 @@ function updateGravity(dt: number, state: Game) {
 }
 export function update(dt: number, state: Game) {
   updateGravity(dt / 1000, state)
+  checkPlayerPlatformCollision(state)
 }
